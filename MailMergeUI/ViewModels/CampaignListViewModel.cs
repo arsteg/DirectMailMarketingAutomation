75df36305f9385930cebddd198bffdefe9c6e845
Mohd Rafi <dotnetexpertdev@gmail.com>
2025-11-03 17:34:02 +0530

    UI improvements

--- MailMergeUI/ViewModels/CampaignListViewModel.cs
+++ MailMergeUI/ViewModels/CampaignListViewModel.cs
@@ -5,11 +5,8 @@ using MailMergeUI.Models;
 using MailMergeUI.Services;
 using MailMergeUI.Views;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
@@ -18,36 +15,114 @@ namespace MailMergeUI.ViewModels
     public class CampaignListViewModel : BaseViewModel
     {
         private readonly CampaignService _service;
+        private readonly MailMergeDbContext _dbContext;
+        public string PageInfoText => $"Page {CurrentPage} of {TotalPages}";
+
+
         public ObservableCollection<Campaign> Campaigns { get; } = new();
 
+        // Pagination properties
+        private int _currentPage = 1;
+        private int _itemsPerPage = 25;
+        private int _totalPages;
+
+        public int CurrentPage
+        {
+            get => _currentPage;
+            set
+            {
+                if (_currentPage != value)
+                {
+                    _currentPage = value;
+                    OnPropertyChanged();
+                    LoadCampaigns();
+                }
+            }
+        }
+
+        public int ItemsPerPage
+        {
+            get => _itemsPerPage;
+            set
+            {
+                if (_itemsPerPage != value)
+                {
+                    _itemsPerPage = value;
+                    OnPropertyChanged();
+                    LoadCampaigns();
+                }
+            }
+        }
+
+        public int TotalPages
+        {
+            get => _totalPages;
+            private set
+            {
+                _totalPages = value;
+                OnPropertyChanged();
+            }
+        }
+
+        // Commands
         public ICommand NewCommand { get; }
         public ICommand EditCommand { get; }
         public ICommand DeleteCommand { get; }
-        private readonly MailMergeDbContext _dbContext;
+        public ICommand NextPageCommand { get; }
+        public ICommand PrevPageCommand { get; }
 
         public CampaignListViewModel(MailMergeDbContext dbContext)
         {
             _dbContext = dbContext;
             _service = new CampaignService(_dbContext);
-            RefreshCampaigns();
 
+            // Initialize commands
             NewCommand = new RelayCommand(_ => OpenEdit(null));
-            EditCommand = new RelayCommand(c => OpenEdit((Campaign)c!));
+            EditCommand = new RelayCommand(c => OpenEdit((Campaign)c!), c => c is not null);
             DeleteCommand = new RelayCommand(c => Delete((Campaign)c!), c => c is not null);
+            NextPageCommand = new RelayCommand(_ => GoToNextPage(), _ => CurrentPage < TotalPages);
+            PrevPageCommand = new RelayCommand(_ => GoToPrevPage(), _ => CurrentPage > 1);
+
+            LoadCampaigns();
         }
 
-        private void RefreshCampaigns()
+        private void LoadCampaigns()
         {
             if (_service == null)
                 return;
+
             Campaigns.Clear();
-            foreach (var c in _service?.Campaigns) Campaigns.Add(c);
+
+            var allCampaigns = _service.Campaigns.ToList();
+            TotalPages = (int)Math.Ceiling((double)allCampaigns.Count / ItemsPerPage);
+
+            var pagedCampaigns = allCampaigns
+                .Skip((CurrentPage - 1) * ItemsPerPage)
+                .Take(ItemsPerPage)
+                .ToList();
+
+            foreach (var c in pagedCampaigns)
+                Campaigns.Add(c);
+
+            TotalPages = (int)Math.Ceiling((double)allCampaigns.Count / ItemsPerPage);
+            if (CurrentPage > TotalPages)
+                CurrentPage = TotalPages;
+
+            OnPropertyChanged(nameof(Campaigns));
+            OnPropertyChanged(nameof(CurrentPage));
+            OnPropertyChanged(nameof(TotalPages));
+
+            OnPropertyChanged(nameof(PageInfoText));
+            OnPropertyChanged(nameof(CurrentPage));            
+            OnPropertyChanged(nameof(allCampaigns.Count)); 
+
         }
 
         private void OpenEdit(Campaign? campaign)
         {
             var vm = new CampaignEditViewModel(campaign, _service);
-            vm.OnSaved += (_) => RefreshCampaigns();
+            vm.OnSaved += (_) => LoadCampaigns();
+
             var window = new CampaignEditWindow(vm);
             window.ShowDialog();
         }
@@ -58,8 +133,26 @@ namespace MailMergeUI.ViewModels
             {
                 _service.Campaigns.Remove(campaign);
                 _service.SaveCampaigns();
-                RefreshCampaigns();
+                LoadCampaigns();
             }
         }
+
+        private void GoToNextPage()
+        {
+            if (CurrentPage < TotalPages)
+            {
+                CurrentPage++;
+                LoadCampaigns();
+            }
+        }
+
+        private void GoToPrevPage()
+        {
+            if (CurrentPage > 1)
+            {
+                CurrentPage--;
+                LoadCampaigns();
+            }
+        }       
     }
 }
