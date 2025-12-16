25ebc04f0cf76dddb8fbae658db1e55565d63a8f
Aanchal Nirdosh <aanchal.nirdosh@itcsystems.com>
2025-12-16 17:18:46 +0530

    fixed  auto reload LoadPendingCountAsync on dashboard

--- MailMergeUI/ViewModels/MainWindowViewModel.cs
+++ MailMergeUI/ViewModels/MainWindowViewModel.cs
@@ -43,7 +43,7 @@ namespace MailMergeUI.ViewModels
                     OnPropertyChanged();
                     if (_activeCampaign != null)
                     {
-                        _ = LoadPendingCountAsync();
+                   //     _ = LoadPendingCountAsync();
                     }
                 }
             }
